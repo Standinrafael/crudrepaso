ed12ba0ea5b383169e2b32774fb736c3fc6ea0fc
Jorge Sanchez <50539993+Standinrafael@users.noreply.github.com>
2024-01-03 23:54:44 -0500

    Holi

---
+++ cdi-jpa-rest/src/main/java/com/distribuida/servicios/ServicioPersonaImpl.java
@@ -0,0 +1,80 @@
+package com.distribuida.servicios;
+
+import com.distribuida.db.Persona;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ServicioPersonaImpl implements ServicioPersona {
+    @Inject
+    EntityManager em;
+
+    @Override
+    public List<Persona> findAll() {
+        return em.createQuery("select o from Persona o")
+                .getResultList();
+    }
+
+    public Persona findById(Integer id) {
+
+        return em.find(Persona.class, id);
+    }
+
+    @Override
+    public boolean borrar(Integer id) {
+        var tx = em.getTransaction();
+        try {
+            tx.begin();
+
+            Persona persona = findById(id);
+
+            if (persona != null) {
+                // Si la persona existe, elimínala
+                em.remove(persona);
+                tx.commit(); // Completar la transacción si la eliminación fue exitosa
+                return true; // Retornar true indicando que la operación fue exitosa
+            } else {
+                System.out.println("La persona con ID " + id + " no fue encontrada.");
+                tx.rollback(); // Revertir la transacción si la persona no fue encontrada
+                return false; // Retornar false indicando que la operación no fue exitosa
+            }
+        } catch (Exception ex) {
+            tx.rollback(); // Revertir la transacción en caso de error
+
+            return false; // Retornar false indicando que la operación no fue exitosa
+        }
+    }
+
+
+    @Override
+    public boolean actualizar(Persona persona) {
+        var tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.merge(persona);
+            tx.commit();
+            return true; // Retorna true indicando que la actualización fue exitosa
+        } catch(Exception ex) {
+            tx.rollback();
+            return false; // Retorna false indicando que ocurrió un error durante la actualización
+        }
+    }
+
+
+    public void insert(Persona p) {
+        var tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(p);
+            tx.commit();
+        }
+        catch(Exception ex) {
+            tx.rollback();
+        }
+    }
+}
